1fc40abec6903e8f872af473d2702a2fd8b25ce3
wangqiyun <770368395@qq.com>
2018-12-19 15:56:07 +0800

    vue communication Test

---
+++ src/components/communication.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p>child:</p>
+        <button @click="sendChildInfo">发送</button>
+        <p>{{childInfo}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'communication',
+    props:{
+        Info:String|Number
+    },
+    data(){
+        return {
+            childInfo : this.Info,
+            selfInfo:'this is selfinfo',
+            count:0,
+        }
+    },
+    methods:{
+        sendChildInfo(){
+            this.count++;
+            this.$emit('send',this.count);
+        }
+    },
+    watch:{
+        Info(newValue,oldValue){
+            this.childInfo=newValue;
+        }
+    }
+}
+</script>
+
--- src/components/newcomponent.vue
+++ src/components/newcomponent.vue
@@ -8,11 +8,18 @@
                 </lichild>
             </li>
         </ul>
+
+        <div>
+            <h4>父子通信</h4><button @click="changeInfo">change</button>
+            <communication :Info="parentInfo" @send="addItem"></communication>
+        </div>
+
     </div>
 </template>
 
 <script>
 import lichild from "@/components/lichild";
+import communication from "@/components/communication";
 
 export default {
     name:'newcomponent',
@@ -20,11 +27,26 @@ export default {
         return {
             items:[
             {name:"name1",show:true},
-            {name:"name2",show:false}]
+            {name:"name2",show:false}],
+
+            parentInfo:`this is parent's infomation 233`,
+
+            count:0,
         }
     },
     components:{
-        lichild
+        lichild,
+        communication
+    },
+    methods:{
+        changeInfo(){
+            this.count++;
+            this.parentInfo=this.count;
+            console.log(this.parentInfo);
+        },
+        addItem(num){
+            this.items.push( {name:num,show:true} );
+        }
     }
 }
 </script>
